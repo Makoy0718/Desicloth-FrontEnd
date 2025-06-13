ce0f2c1342643d0085f6c5890524237bf29f5289
Brilrs <bricm304@gmail.com>
2025-06-13 08:39:18 -0500

    se desarrollo la tabla role, listar

---
+++ src/app/models/producto.ts
@@ -0,0 +1,8 @@
+export class Producto {
+    idProducto:number=0
+    nombreProducto:string=""
+    descripcionProducto:string=""
+    tipoProducto:string=""
+    tallaProducto:string=""
+    precioProducto:number=0
+}
