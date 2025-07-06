095555fc5252a0334f4119545f48c8d1185c1d97
Brilrs <bricm304@gmail.com>
2025-07-06 14:30:51 -0500

    Componente de registro pago corregido

--- src/app/services/pago.service.ts
+++ src/app/services/pago.service.ts
@@ -3,6 +3,8 @@ import { environment } from '../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pago } from '../models/pago';
 import { Observable, Subject } from 'rxjs';
+import { Pedido } from '../models/pedido';
+import { UsuarioMontoDTO } from '../models/UsuarioMontoDTO';
 
 
 const base_url = environment.base;
@@ -21,12 +23,12 @@ export class PagoService {
     return this.http.get<Pago[]>(`${this.url}/listarPago`);
   }
 
-  insertPago(pagnuv: Pago): Observable<void> {
-    return this.http.post<void>(`${this.url}/insertarPago`, pagnuv);
+  insertPago(pago: Pago): Observable<void> {
+    return this.http.post<void>(`${this.url}/insertarPago`, pago);
   }
 
-  updatePago(pagmod: Pago): Observable<void> {
-    return this.http.put<void>(`${this.url}/modificarPago`, pagmod);
+  updatePago(pago: Pago): Observable<void> {
+    return this.http.put<void>(`${this.url}/modificarPago`, pago);
   }
 
   deletePago(id: number): Observable<void> {
@@ -44,6 +46,22 @@ export class PagoService {
   listIdPago(id:number){
     return this.http.get<Pago>(`${this.url}/buscarPago/${id}`)
   }
+
+  //metodos para hacer el componte en buscar 
+  buscarPorMetodo(metodo:string): Observable<Pago[]>{
+    const params = new HttpParams().set('metodo',metodo);
+    return this.http.get<Pago[]>(`${this.url}/buscarPorMetodo`, {params});
+  }
+  //query1:top 5 pagos mas altos:
+  obtenerTop5Pagos():Observable<Pago[]>{
+    return this.http.get<Pago[]>(`${this.url}/top5pagos`)
+  }
+   
+  //quer2:obtener total de pagos por usuario
+  obtenerTotalPagosPorUsuario():Observable<UsuarioMontoDTO[]>{
+    return this.http.get<UsuarioMontoDTO[]>(`${this.url}/totalPorUsuario`)
+  }
+
 
   
 }
